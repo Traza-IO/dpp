07103826d7d727062728d1e8cbbc4861090b7e49
softpedro <pedromollehuanca@gmail.com>
2024-11-15 18:54:19 -0500

    update home

--- app/components/Materials/Materials.tsx
+++ app/components/Materials/Materials.tsx
@@ -2,27 +2,33 @@
 
 import Image from "next/image";
 import imgBussiness from "../../assets/logo-mestiza.svg";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
 
 const Materials = () => {
   return (
-    <div className="flex items-start justify-between max-w-[1024px] mx-auto mt-4 max-md:flex-col px-4">
+    <div className="flex items-start justify-between max-w-[1024px] mx-auto mt-4 px-4">
       <div className="max-md:text-center max-md:mb-4 w-full">
-        <h4 className="text-slate-400 font-bold text-[18px]">Materiales</h4>
-        <ul>
-          <li className="text-[14px]">100% algodón tangüis</li>
-          <li className="text-[14px]">100% tintes ecológicos</li>
+        <h4 className="font-bold text-[#5f6259] text-[15px] text-left">
+          Materiales
+        </h4>
+        <ul className="p-0 text-left">
+          <li className="text-[11px] text-[#5f6259]">100% algodón tangüis</li>
+          <li className="text-[11px] text-[#5f6259]">100% tintes ecológicos</li>
         </ul>
       </div>
       <div className="max-md:text-center max-md:mb-4 w-full">
-        <h4 className="text-slate-400 font-bold text-[18px] uppercase">
+        <h4 className="font-bold text-[#5f6259] text-[15px] uppercase">
           hooded sweatshirt
         </h4>
         <ul>
-          <li className="text-[14px]">Trazable</li>
+          <li className="text-[14px] mt-3">Trazable</li>
         </ul>
       </div>
       <div className="max-md:text-center max-md:mb-4 w-full">
-        <h4 className="text-slate-400 font-bold text-[18px]">Empresa</h4>
+        <h4 className="font-bold text-[#5f6259] text-[15px]">Empresa</h4>
         <figure>
           <Image
             src={imgBussiness}
@@ -31,6 +37,20 @@ const Materials = () => {
             className="block mx-auto"
           />
         </figure>
+        <div className="w-full flex justify-between max-w-[90px] mx-auto mt-2">
+          <button>
+            <FaFacebookSquare color="#acb2a8" size={18} />
+          </button>
+          <button>
+            <FaInstagram color="#acb2a8" size={18} />
+          </button>
+          <button>
+            <FaWhatsapp color="#acb2a8" size={18} />
+          </button>
+          <button>
+            <IoCart color="#acb2a8" size={18} />
+          </button>
+        </div>
       </div>
     </div>
   );
